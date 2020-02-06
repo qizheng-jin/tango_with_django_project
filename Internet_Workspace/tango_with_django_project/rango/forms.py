0e3de5c2d259b2f5eadece2a8e03c253050dca02
qizheng-jin <59999690+qizheng-jin@users.noreply.github.com>
2020-02-06 23:34:59 +0000

    Chapter9
    Finish doing Chapter9

--- Internet_Workspace/tango_with_django_project/rango/forms.py
+++ Internet_Workspace/tango_with_django_project/rango/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
+
+# We could add these forms to views.py, but it makes sense to split them off into their own file.
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=Category.nameLength,
-                           help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.nameLength, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -15,18 +17,18 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=Page.titleLength,
-                            help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=Page.titleLength, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
 
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
-        # If url is not empty and doesn't start with 'http://',
-        # then prepend 'http://'.
         if url and not url.startswith('http://'):
             url = f'http://{url}'
             cleaned_data['url'] = url
@@ -34,6 +36,15 @@ class PageForm(forms.ModelForm):
         return cleaned_data
 
 
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
-        model = Page
-        exclude = ('category',)
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
